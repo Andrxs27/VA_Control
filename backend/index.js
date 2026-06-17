49948231d16ff32ea50cacc2cd61552224a2f529
Andrxs27 <andrecrack1110@gmail.com>
2026-06-17 15:24:27 -0500

    `Update port number in backend index.js`

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
     res.json({ mensaje: 'Backend VA_Control funcionando' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
