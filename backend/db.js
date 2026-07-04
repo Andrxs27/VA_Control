9c01dad5971fd6cbc2afe97ed0e33d52f9a6e489
Andrxs27 <andrecrack1110@gmail.com>
2026-07-04 17:27:54 -0500

    code

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'thomas.proxy.rlwy.net',
+    database: process.env.DB_NAME || 'railway',
+    password: process.env.DB_PASSWORD || 'ekrLNkiBdBOvQJDeuFtEoIVowQrCTssj',
+    port: process.env.DB_PORT || 17441,
 });
 
 module.exports = pool;
--- backend/seeders/20260530215649-demo-usuarios.js
+++ backend/seeders/20260530215649-demo-usuarios.js
@@ -6,7 +6,7 @@ module.exports = {
       {
         nombre: 'Administrador Principal',
         email: 'admin@vacontrol.com',
-        password: '123456',
+        password: '$2b$10$whoQ8loKx44zRf6Dk.IBde.BXPLoTZ23bAUnsmh2ourJT61ULgdha',
         rol: 'admin',
         activo: true,
         creado_en: new Date(),
@@ -15,7 +15,7 @@ module.exports = {
       {
         nombre: 'Carlos Vendedor',
         email: 'carlos@vacontrol.com',
-        password: '123456',
+        password: '$2b$10$07QS/o.oJyWN9UACaYT5BOseIEoTkzJMZULLOUz33rp5qH2m9ZxcK',
         rol: 'vendedor',
         activo: true,
         creado_en: new Date(),
@@ -24,7 +24,7 @@ module.exports = {
       {
         nombre: 'Maria Tecnico',
         email: 'maria@vacontrol.com',
-        password: '123456',
+        password: '$2b$10$hd1tCN9REH/64IFleDXI4u6nAI.ZatIWB0scKM0f16YtC3fMKYP1q',
         rol: 'tecnico',
         activo: true,
         creado_en: new Date(),
